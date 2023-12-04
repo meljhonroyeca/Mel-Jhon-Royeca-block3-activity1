8c022a38cc27b92c4916ed1b809787eea3beb1fa
meljhon <meab.royeca.up@phinmaed.com>
2023-12-04 20:23:54 +0800

    4th Commit

--- app/src/main/java/com/meljhonroyeca/block3/quiz/MainActivity.kt
+++ app/src/main/java/com/meljhonroyeca/block3/quiz/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.meljhonroyeca.block3.quiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btContinue: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btContinue = findViewById(R.id.bt_next_main)
+
+
     }
 }
